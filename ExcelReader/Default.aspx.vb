5b362d3a2df732f20a73e144d426fd78c7ecfb1a
taywan <deksoke@gmail.com>
2016-05-06 11:11:45 +0700

    อัพโหลดโปรเจ็ค
    อัพโหลดโปรเจ็คเริ่มต้น

---
+++ ExcelReader/Default.aspx.vb
@@ -0,0 +1,17 @@
+﻿Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub btReadData_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btReadData.Click
+        Dim ds As New DataSet
+        Dim dt As System.Data.DataTable
+
+        'Support File Extensions - xls, xlsx, csv
+        Using ex As New ExcelReader("C:/20160401.xls")
+            ds = ex.GetWorkBook.ToDataSet(False, True)
+            dt = ds.Tables(0)
+        End Using
+
+        dgv.DataSource = dt
+        dgv.DataBind()
+    End Sub
+End Class
